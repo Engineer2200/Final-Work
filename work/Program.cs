d3641fc007d669b77bb6b3a4694fbd9a8fedebcb
Engineer <arsenya.suvorov@mail.ru>
2022-12-27 13:06:41 +0400

    Sorted and created a second array

--- work/Program.cs
+++ work/Program.cs
@@ -2,3 +2,26 @@
 Console.WriteLine("Введите массив строк через пробел");
 string stroka = Console.ReadLine();
 arrayinitial = stroka.Split(' ');
+string[] arrayfinal = new string[arrayinitial.Length];
+void SecondArrayWithIF(string[] arrayinitial, string[] arrayfinal)
+{
+    int count = 0;
+    for (int i = 0; i < arrayinitial.Length; i++)
+    {
+    if(arrayinitial[i].Length <= 3)
+        {
+        arrayfinal[count] = arrayinitial[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arrayinitial, arrayfinal);
+PrintArray(arrayfinal);
